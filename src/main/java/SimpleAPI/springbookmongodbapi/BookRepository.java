5868ccea59d6077e33d4dfde01288c7778de4788
Thomas Roche <troche@linwoodhomes.com>
2023-11-26 01:36:20 -0800

    test

--- src/main/java/SimpleAPI/springbookmongodbapi/BookRepository.java
+++ src/main/java/SimpleAPI/springbookmongodbapi/BookRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BookRepository extends MongoRepository<Book, String> {
     // Additional custom queries can be added here
+    //just to add a comment
 }
